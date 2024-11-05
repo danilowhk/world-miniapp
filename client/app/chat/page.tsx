97abb24dfae1dce4d610f9c3bef9bb93d2dbfa9b
dzns <davidkim1131@gmail.com>
2024-11-05 13:06:14 +0700

    added lessons fetch method in chat initial page

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -1,13 +1,35 @@
-// app/chat/page.tsx
-"use client";
-
-// import ChatSection from "@/components/Conversation";
 import LessonsSection from "@/components/Sections/StartLesson";
 
-export default function ChatPage() {
+const database_url = process.env.DATABASE_URL;
+
+export default async function ChatPage() {
+  
+  let lessons = [];
+
+  try {
+    const response = await fetch(`${database_url}/lessons/user`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userId: "",
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch lessons");
+    }
+
+    lessons = await response.json();
+  } catch (error) {
+    console.error("Error fetching lessons:", error);
+    // Optionally, you can set a fallback value for lessons or render an error message
+    lessons = [];
+  }
   return (
     <div className="flex flex-col h-screen bg-gray-100 pb-12">
-        <LessonsSection />
+      <LessonsSection lessons={lessons} />
     </div>
   );
 }
