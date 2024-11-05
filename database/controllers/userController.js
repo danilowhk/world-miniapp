b799624e2c6dcf7564b4ef76182f1106f9c30e3d
dzns <davidkim1131@gmail.com>
2024-11-05 12:12:00 +0700

    added update user info

--- database/controllers/userController.js
+++ database/controllers/userController.js
@@ -34,4 +34,24 @@ const findUserById = async (req, res) => {
   }
 };
 
-module.exports = { createUser, listUsers, findUserById };
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { createUser, listUsers, findUserById, updateUser };
