fffa1e4e9bb388eb6e3d15f0d93a9b85884c6ece
dzns <davidkim1131@gmail.com>
2024-11-07 18:08:34 +0700

    ajusted chat page when use have lessons created

--- client/components/Sections/StartLesson/index.tsx
+++ client/components/Sections/StartLesson/index.tsx
@@ -19,6 +19,29 @@ export default function LessonsSection({ lessons, loading }: { lessons: any, loa
       <div className="h-screen bg-gradient-to-b from-white to-blue-100 py-6 flex flex-col -mb-12">
         <LessonsHeader />
         <LessonsTable lessons={lessons} />
+        <div className="flex flex-col px-6 pt-24">
+          <div className="flex-grow flex flex-col justify-center items-center space-y-8">
+            <div className="p-3 bg-gradient-to-br from-blue-400 to-blue-700 rounded-full animate-flip-animation">
+              <BadgePlus size={40} className="text-white" />
+            </div>
+            <h2 className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+              Unlock new leagues
+            </h2>
+            <p className="max-w-md text-center text-lg text-gray-600">
+              Finish more lessons to unlock higher leagues.
+            </p>
+            <a
+              href="/chat/roleplays"
+              className="rounded-full bg-gradient-to-br from-blue-400 to-blue-700 px-8 py-3 text-lg font-semibold text-white shadow-lg hover:from-blue-600 hover:to-purple-600 transition-all "
+            >
+              New Lesson
+            </a>
+          </div>
+          <div className="mt-auto pt-16 text-purple-700 flex items-center justify-center">
+            <TimerReset size={20} className="mr-2 text-purple-700" />
+            Ends on {dayOfWeek} at 11:59PM UTC
+          </div>
+        </div>
       </div>
     )
   } else {
